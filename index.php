884fb8a4306659254a8627054972d25f4ca3bb1a
lalabhi <abhishek.lal@qed42.com>
2019-01-17 16:55:09 +0530

    added the backend

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+echo $_POST['fname'];
+echo  $_POST['lname'];
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+$data = [
+    'fname' => $_POST['fname'],
+    'lname' => $_POST['lname'],
+    'email'=>$_POST['email'],
+    'phone'=>$_POST['phone'],
+    'gender' => $_POST['gender'],
+    'addr' => $_POST['addr'],
+    'bday' =>$_POST['bday'],
+    'quali' =>$_POST['quali'],
+
+];
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=info", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    $sql="INSERT INTO jobInfo (fname, lname,email,phone ,gender, addr, bday, quali) VALUES (:fname, :lname, :email, :phone, :gender, :addr, :bday, :quali)";
+    $stmt = $conn->prepare($sql); 
+    $stmt->execute($data);
+
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+    $conn = null;
+
+?>
